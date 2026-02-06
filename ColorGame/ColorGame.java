e08d5e225308fc638f0ba8a0f0b2acae4222568e
Azriel03 <richmondadan22@gmail.com>
2026-02-06 20:49:22 +0800

    Implement ColorGame activity with game logic

---
+++ ColorGame/ColorGame.java
@@ -0,0 +1,144 @@
+package com.example.colorgame;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ColorGame extends AppCompatActivity {
+
+    TextView tvColorWord, tvScore, tvTimer;
+    Button btnBlue, btnPurple, btnGreen, btnYellow, btnRed, btnBrown;
+    Button btnBack;
+    ArrayList<ColorItem> colorList = new ArrayList<>();
+    int score = 0;
+    int textDuration = 2000;
+    Handler handler = new Handler();
+    Runnable textRunnable;
+    int roundTime = 60000;
+    int round = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.colorgame);
+
+        tvColorWord = findViewById(R.id.tvColorWord);
+        tvScore = findViewById(R.id.tvScore);
+        tvTimer = findViewById(R.id.tvTimer);
+
+        btnBlue = findViewById(R.id.btnBlue);
+        btnPurple = findViewById(R.id.btnPurple);
+        btnGreen = findViewById(R.id.btnGreen);
+        btnYellow = findViewById(R.id.btnYellow);
+        btnRed = findViewById(R.id.btnRed);
+        btnBrown = findViewById(R.id.btnBrown);
+
+        btnBack = findViewById(R.id.btnBack);
+
+        colorList.add(new ColorItem("BLUE", Color.BLUE));
+        colorList.add(new ColorItem("PURPLE", Color.parseColor("#9C27B0")));
+        colorList.add(new ColorItem("GREEN", Color.GREEN));
+        colorList.add(new ColorItem("YELLOW", Color.YELLOW));
+        colorList.add(new ColorItem("RED", Color.RED));
+        colorList.add(new ColorItem("BROWN", Color.parseColor("#795548")));
+
+        setupButton(btnBlue);
+        setupButton(btnPurple);
+        setupButton(btnGreen);
+        setupButton(btnYellow);
+        setupButton(btnRed);
+        setupButton(btnBrown);
+
+        OnBackPressedCallback backCallback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                finish();
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(this, backCallback);
+
+        btnBack.setOnClickListener(v -> {
+            backCallback.handleOnBackPressed();
+        });
+
+        startRound();
+    }
+
+    private class ColorItem {
+        String name;
+        int colorValue;
+
+        ColorItem(String name, int colorValue) {
+            this.name = name;
+            this.colorValue = colorValue;
+        }
+    }
+
+    void setupButton(Button button) {
+        button.setOnClickListener(v -> {
+            String selected = button.getText().toString();
+            String correct = tvColorWord.getText().toString();
+
+            if (selected.equals(correct)) {
+                score++;
+                tvScore.setText("Score: " + score);
+            }
+        });
+    }
+
+    void showRandomText() {
+        Random random = new Random();
+        ColorItem wordItem = colorList.get(random.nextInt(colorList.size()));
+        ColorItem colorItem = colorList.get(random.nextInt(colorList.size()));
+
+        tvColorWord.setText(wordItem.name);
+        tvColorWord.setTextColor(colorItem.colorValue);
+    }
+
+    void startTextLoop() {
+        textRunnable = new Runnable() {
+            @Override
+            public void run() {
+                showRandomText();
+                handler.postDelayed(this, textDuration);
+            }
+        };
+        handler.post(textRunnable);
+    }
+
+    void startRound() {
+        startTextLoop();
+        startRoundTimer();
+    }
+
+    void startRoundTimer() {
+        new CountDownTimer(roundTime, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tvTimer.setText("Time: " + millisUntilFinished / 1000 + "s");
+            }
+
+            @Override
+            public void onFinish() {
+                nextRound();
+            }
+        }.start();
+    }
+
+    void nextRound() {
+        round++;
+        textDuration -= 300;
+        if (textDuration < 500) textDuration = 500;
+
+        handler.removeCallbacks(textRunnable);
+        startRound();
+    }
+}
